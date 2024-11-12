d8934c3268856c127028b222f1ffd21539aed26a
ForzaCesarGarcia <julio.garcia@forzatrans.com>
2024-11-12 15:07:42 -0600

    Adding configuration files

--- src/data/preprocess.py
+++ src/data/preprocess.py
@@ -2,6 +2,7 @@ import pandas as pd
 import os
 from sklearn.preprocessing import StandardScaler, LabelEncoder
 from sklearn.decomposition import PCA
+import joblib
 
 class DataPreprocessor:
     def __init__(self, raw_data_path, processed_data_path):
@@ -15,38 +16,84 @@ class DataPreprocessor:
         self.data = pd.read_csv(self.raw_data_path)
         return self.data
 
-    def preprocess(self):
-        processed_data = self.data.copy()
+    def preprocess(self, train = True, processed_data = None):
+        if train:
+            processed_data = self.data.copy()
 
         # Convertir la columna 'date' a datetime y crear una columna 'month'
         processed_data['date'] = pd.to_datetime(processed_data['date'], format="%d/%m/%Y %H:%M")
         processed_data['month'] = processed_data['date'].dt.month_name()
 
         # Variables numéricas y categóricas
-        numeric_variables = processed_data.select_dtypes(include='number').columns.to_list()
-        object_variables = processed_data.select_dtypes(include='object').columns.to_list()
+    
+        numeric_variables = ['Usage_kWh', 'Lagging_Current_Reactive.Power_kVarh', 'Leading_Current_Reactive_Power_kVarh', 'CO2(tCO2)', 'Lagging_Current_Power_Factor', 'Leading_Current_Power_Factor', 'NSM']
+        object_variables = ['WeekStatus', 'Day_of_week', 'Load_Type', 'month']
 
         if 'Load_Type' in object_variables:
             object_variables.remove('Load_Type')
 
         # Escalar los datos numéricos
-        scaler = StandardScaler()
-        data_scaled = scaler.fit_transform(processed_data[numeric_variables])
+        if not train:
+            print("Loading Scaler")
+            scaler = joblib.load("saved_models/scaler.pkl")
+            data_scaled = scaler.transform(processed_data[numeric_variables])
+        else:
+            print("Training Scaler")
+            scaler = StandardScaler()
+            data_scaled = scaler.fit_transform(processed_data[numeric_variables])
+
+        
+        if train:
+            joblib.dump(scaler, "saved_models/scaler.pkl")
 
         # Reducción de dimensionalidad usando PCA
-        pca = PCA(n_components=0.95)
-        data_pca = pca.fit_transform(data_scaled)
+        
+        if not train:
+            print("Loading PCA")
+            pca = joblib.load("saved_models/pca.pkl")
+            data_pca = pca.transform(data_scaled)
+        else:
+            print("Training PCA")
+            pca = PCA(n_components=0.95)
+            data_pca = pca.fit_transform(data_scaled)
+        
+        if train:
+            joblib.dump(pca, "saved_models/pca.pkl")
         pca_df = pd.DataFrame(data_pca, columns=[f'PC{i+1}' for i in range(data_pca.shape[1])])
 
         # Codificación de variables categóricas
-        data_encoded_df = pd.get_dummies(processed_data[object_variables], columns=object_variables, drop_first=True)
+        if train:
+            data_encoded_df = pd.get_dummies(processed_data[object_variables], columns=object_variables, drop_first=True)
+        else:
+            data_encoded_df = pd.get_dummies(processed_data[object_variables], columns=object_variables, drop_first=False)
+
+        dummy_columns = ['WeekStatus_Weekend', 'Day_of_week_Monday', 'Day_of_week_Saturday',
+                            'Day_of_week_Sunday', 'Day_of_week_Thursday', 'Day_of_week_Tuesday',
+                            'Day_of_week_Wednesday', 'month_August', 'month_December',
+                            'month_February', 'month_January', 'month_July', 'month_June',
+                            'month_March', 'month_May', 'month_November', 'month_October',
+                            'month_September']
+        
+        if not train:
+            
+            # Asegurarse de que los datos de entrada tengan las mismas columnas
+            for col in dummy_columns:
+                if col not in data_encoded_df.columns:
+                    data_encoded_df[col] = False
+        
+        data_encoded_df = data_encoded_df[dummy_columns]
+
 
         # Concatenación del dataframe codificado y el PCA
         X = pd.concat([data_encoded_df, pca_df], axis=1)
 
-        # Codificación de la variable objetivo
-        le = LabelEncoder()
-        y = le.fit_transform(processed_data['Load_Type'])
+        if train:
+            # Codificación de la variable objetivo
+            le = LabelEncoder()
+            y = le.fit_transform(processed_data['Load_Type'])
+            joblib.dump(le, "saved_models/label_encoder.pkl")
+        else:
+            y = None
 
         return X, y
 
